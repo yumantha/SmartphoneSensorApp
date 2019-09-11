127adb694f9c259df137815290b276f81ecf017d
Yumantha <yumanthadesilva@gmail.com>
2019-09-11 07:47:43 +0530

    first commit

---
+++ app/src/main/java/com/yumantha/smartphonesensorapp/MainActivity.java
@@ -0,0 +1,78 @@
+package com.yumantha.smartphonesensorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
+
+    private SensorManager sensorManager;
+    Sensor accelerometer;
+
+    TextView xValue, yValue, zValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+        Button stopButton = (Button) findViewById(R.id.stopButton);
+
+        xValue = (TextView) findViewById(R.id.xValue);
+        yValue = (TextView) findViewById(R.id.yValue);
+        zValue = (TextView) findViewById(R.id.zValue);
+
+        startButton.setOnClickListener((View.OnClickListener) this);
+        stopButton.setOnClickListener((View.OnClickListener) this);
+
+        Log.d(TAG, "onCreate: Initializing sensor services");
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(MainActivity.this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        Log.d(TAG, "onCreate: Registered accelerometer listener");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.startButton:
+                sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+                break;
+            case R.id.stopButton:
+                sensorManager.unregisterListener(this);
+
+                xValue.setText("X Value: 0.0");
+                yValue.setText("Y Value: 0.0");
+                zValue.setText("Z Value: 0.0");
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        Log.d(TAG, "onSensorChanged: X:" + event.values[0] + " Y:" + event.values[1] + " Z:" + event.values[2]);
+
+        xValue.setText("X Value:\t\t" + event.values[0]);
+        yValue.setText("Y Value:\t\t" + event.values[1]);
+        zValue.setText("Z Value:\t\t" + event.values[2]);
+    }
+
+
+}
